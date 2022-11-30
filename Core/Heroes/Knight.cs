f682000d0b2a9ed0d327f7646ecc8a93fe698c0d
Skaram20c <118626675+Skaram20c@users.noreply.github.com>
2022-11-29 21:24:27 -0500

    Fixed Hero class attack names and descriptions

--- Core/Heroes/Knight.cs
+++ Core/Heroes/Knight.cs
@@ -25,79 +25,108 @@ namespace IOT1030_Phase2_GUI.Core.Heroes
             };
             _attackDescriptions = new List<string>()
             {
-                "Simple attack with equipped weapon",
-                "Special powerful knight attacks that deals a massive damage",
-                "Lucky knight heals himself",
-                "Knight enrages"
+                "Simple Attack depending on strength.",
+                "Rigorous Night with unbeatable strength",
+                "One hit Kill Smash",
+                "Luck have unexpected improvements"
             };
         }
         //public Knight() : base(PlayerName.Knight) { }
 
-        protected override int NormalAttack()
+        public int Thunderbolt()
         {
-            Console.WriteLine(_strength);
+            Armour armour = equipItem.Armour;
+            bool equiparmour = equipItem.Equip(armour);
+            if (equiparmour)
+            {
+                return _strength += (armour.GetHitpoint() + _powerUp);
+            }
+            return thunderboltstrike;
+        }
+        
+        public int KnockoutSmash()
+        {
+            Sword sword = equipItem.Sword;
+            bool equipsword = equipItem.Equip(sword);
+            if (equipsword)
+            {
+                return (sword.GetDamage() + _powerUp);
+            }
+            return Thunderbolt();
+        }
+        
+        public int UnexpectedLuck()
+        {
+            int min = 0;
+            int times = 2;
+            if (_luck <= MaxHealth/4 && _luck >= min)
+            {
+                Map map = equipItem.Map;
+                bool equipMap = equipItem.Equip(map);
+                if (equipMap)
+                {
+                    _strength += _strength;
+                    _powerUp += _strength;
+                    _strength += _powerUp;
+                    if(_health <= _luck)
+                    {
+                        _health += _health;
+                    }
+                }
+                if (!equipMap)
+                {
+                    return RageKnight(_luck + _luck);
+                }
+            }
+            return (_strength * times);
+        }
+        
+        public override int NormalAttack()
+        {
+            int min = 0;
+            int max = 100;
+            if(_health <= max && _health >= max / 2)
+            {
+                return (_strength * 2) + _powerUp;
+            }
+            if(_health < max/2 && _health > min)
+            {
+                return _strength + _powerUp;
+            }
             return _strength;
         }
-
-        public PlayerName GetKnightName() { return _name; }
-        public int GetKnightStrength() { return _strength; }
-        public void SetKnightStrength(int strength) { _strength = strength; }
-        public int GetKnightPower() { return _powerUp; }
-        public void SetKnightPower(int powerUp) { _powerUp = powerUp; }
-        public int GetKnightLuck() { return _luck; }
-        public void SetKnightLuck(int luck) { _luck = luck; }
-        public int GetKnightStealth() { return _stealth; }
-        public void SetKnightStealth(int stealth) { _stealth = stealth; }
-
+        
         public int RageKnight(int rageamount)
         {
             const int MaxRageamount = 100;
+            RagePotion ragepotion = equipItem.RagePotion;
+            bool equipRage = equipItem.UseRagePotion();
+            if(rageamount <= MaxRageamount && rageamount >= MaxRageamount/2)
+            {
+                if (PlayerHasItem(ragepotion) || equipRage)
+                {  
+                    _strength += ragepotion.GetPowerUp();
+                    _powerUp += ragepotion.GetPowerUp();
+                    return (_powerUp + _strength);
+                }
+            }
+            if (rageamount > 0 && rageamount < MaxRageamount/2)
+            {
+                if(_health <= rageamount)
+                {
+                    if(PlayerHasItem(ragepotion) || equipRage)
+                    {
+                        _health += ragepotion.Getheal();
+                    }
+                }
+                Heal(rageamount);
+                return _strength + (_powerUp / 2);
+            }
             if (rageamount <= 0)
             {
-                rageamount = (_strength) + (_powerUp * 2);
-            }
-            else if (rageamount > 0 && rageamount <= 50)
-            {
-                return rageamount = (_strength + _powerUp / 2);
-            }
-            else if (rageamount <= MaxRageamount)
-            {
-                return _stealth = rageamount;
-            }
-            else
-            {
-                return rageamount = MaxRageamount;
+                return _strength;
             }
             return rageamount;
-        }
-        public int KnockoutSmash()
-        {
-            return (2 * _strength) + _powerUp;
-        }
-
-        public int UnexpectedLuck()
-        {
-            if (_luck <= 10)
-            {
-                if (_health <= 10)
-                {
-                    _health = MaxHealth;
-                }
-                else if (_health > 10 && _health <= 50)
-                {
-                    Heal(_luck);
-                    _health++;
-                }
-                else
-                {
-                    Damage(_luck);
-                }
-            }
-            else
-            {
-                Heal(_luck);
-            }
-            return _health;
         }
 
         public override string ToString()
