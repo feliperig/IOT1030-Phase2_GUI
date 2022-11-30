2b7197b3d51568d6edd6ef5d4d18ece66b6f7f8a
Skaram20c <118626675+Skaram20c@users.noreply.github.com>
2022-11-29 21:45:56 -0500

    Required for Updated player class

---
+++ Core/Inventory.cs
@@ -0,0 +1,81 @@
+class Inventory
+    {
+        private List<InventoryItem> itemsList = new List<InventoryItem>();
+        
+        public bool HasMap()
+        {
+            foreach (InventoryItem item in itemsList)
+            {
+                if (item._name.Equals(InventoryItemEnum.Map))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasSword()
+        {
+            foreach (InventoryItem item in itemsList)
+            {
+                if (item._name.Equals(InventoryItemEnum.Sword))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void AddItem(InventoryItem item)
+        {
+            itemsList.Add(item);
+        }
+
+        public bool ItemInInventory(InventoryItem item)
+        {
+            foreach (InventoryItem element in itemsList)
+            {
+                if (element._name.Equals(item._name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void RemoveItem(InventoryItem item)
+        {
+            bool check = false;
+            int index = -1;
+            foreach (var elements in itemsList)
+            {
+                if (elements._name == (item._name))
+                {
+                    check = true;
+                }
+            }
+            if (check)
+            {
+                for (int i = 0; i < itemsList.Count; i++)
+                {
+                    if (itemsList[i]._name == (item._name))
+                    {
+                        index = i;
+                    }
+                }
+                itemsList.RemoveAt(index);
+            }
+        }
+
+        public override string ToString()
+        {
+            string ret = " ";
+            foreach (var item in itemsList)
+            {
+                ret = ret + item.ToString();
+            }
+            return ret;
+        }
+    }
+
+    enum InventoryItemEnum { Sword, Bow, Shield, HealingPotion, RagePotion, InvisiblePotion, Map, Armour, MagicStick }
