56a06b6627acabddf389d29ad3fca2374a0a6245
Felipe Rocha <A00249642@mycambrian.ca>
2022-12-02 13:09:01 -0500

    add goblin monster and modifications to monsters base class

---
+++ Core/Monsters/Goblin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IOT1030_Phase2_GUI.Core.Monsters
+{
+    class Goblin : Monster
+    {
+        public Goblin() 
+        {
+            _maxHealth = ( rnd.Next()*15 ) + 45;
+            _currentHealth = _maxHealth;
+        }
+
+        public override int DealDamage()
+        {
+            return ( rnd.Next()*10 ) + 1;
+        }
+
+        public override void TakeAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Core/Monsters/Monster.cs
+++ Core/Monsters/Monster.cs
@@ -8,6 +8,8 @@ namespace IOT1030_Phase2_GUI.Core.Monsters
 {
     public abstract class Monster
     {
+        protected readonly Random rnd = new Random();
+
         /// <summary>
         /// The maximum health of the hero
         /// </summary>
@@ -21,24 +23,39 @@ namespace IOT1030_Phase2_GUI.Core.Monsters
         public int CurrentHealth { get { return _currentHealth; } }
 
         /// <summary>
+        /// Defines if the monster is dead due to its current health
+        /// </summary>
+        protected bool _isDead = false;
+        public bool IsDead
+        {
+            get { return _isDead; }
+            set { _isDead = _currentHealth <= 0; }
+        }
+
+        /// <summary>
         /// Takes the damage given, returns if monster is alive
         /// </summary>
         /// <returns>False if monster is not alive</returns>
         public virtual bool TakeDamage(int damage) {
             _currentHealth -= damage;
 
-            if (_currentHealth <= 0)
-                return false; // Monster dead
-
-            return true; // Monster still alive
+            if (_currentHealth < 0)
+            {
+                _currentHealth = 0;
+                return IsDead; // Monster dead
+            }
+            return IsDead; // Monster still alive
         }
+
+        /// <summary>
+        /// Deals damage to the opponent
+        /// </summary>
+        /// <returns>Damage that should be dealt to the opponent</returns>
+        public abstract int DealDamage();
 
         /// <summary>
         /// Chooses what action the monster should do
         /// </summary>
-        public virtual void TakeAction()
-        {
-            
-        }
+        public abstract void TakeAction();
     }
 }
